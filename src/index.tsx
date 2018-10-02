610a377eb058e74cad4d048bfea15062e8f79001
Roman Schaller <roman.schaller@gmail.com>
2018-10-02 22:25:12 +0200

    Use IndexedDB

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,36 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
-import Hello from './containers/Hello';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { enthusiasm } from './reducers/index';
-import { StoreState } from './types/index';
-import { EnthusiasmAction } from './actions';
-import { offline } from '@redux-offline/redux-offline';
-import offlineConfig from '@redux-offline/redux-offline/lib/defaults';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
+import Hello from "./containers/Hello";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { enthusiasm } from "./reducers/index";
+import { StoreState } from "./types/index";
+import { EnthusiasmAction } from "./actions";
+import { offline } from "@redux-offline/redux-offline";
+import offlineConfig from "@redux-offline/redux-offline/lib/defaults";
+import * as localforage from "localforage";
 
 const initialState: StoreState = {
   enthusiasmLevel: 1,
-  languageName: 'TypeScript',
+  languageName: "TypeScript"
 };
 
+console.log("offline config", offlineConfig);
+offlineConfig.persistOptions = { storage: localforage };
 const offlineEnhancer: any = offline(offlineConfig);
 
 const store = createStore<StoreState, EnthusiasmAction, any, any>(
   enthusiasm,
   initialState,
-  offlineEnhancer);
+  offlineEnhancer
+);
 
 ReactDOM.render(
   <Provider store={store}>
     <Hello />
   </Provider>,
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 registerServiceWorker();
